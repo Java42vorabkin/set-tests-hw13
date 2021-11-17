2b3cf9407e732825de2b3751ded83bc03e19ce72
Yuragranovsky <yuragranovsky@gmail.com>
2021-11-17 15:45:19 +0200

    hw #12 adds tests for iterator functionality

--- src/telran/util/SetTests.java
+++ src/telran/util/SetTests.java
@@ -2,10 +2,14 @@ package telran.util;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SetTests {
+private static final int N_RANDOM_NUMBERS = 10000;
 Integer[] initialNumbers = {
 	10, 20, 40, 60	
 };
@@ -16,10 +20,26 @@ Set<Integer> set;
 		fillSet();
 	}
 
-	private void fillSet() {
-		for(Integer num: initialNumbers) {
-			set.add(num);
+	private  void fillSet() {
+		fillSetFromArray(set, initialNumbers);
+		
+		
+	}
+
+	private <T> void fillSetFromArray(Set<T> res, T[] array) {
+		
+		for(T num: array) {
+			res.add(num);
 		}
+		
+	}
+	
+	private Integer[] getRandomNumbers() {
+		Integer[] res = new Integer[N_RANDOM_NUMBERS];
+		for (int i = 0; i < res.length; i++) {
+			res[i] = (int) (Math.random() * Integer.MAX_VALUE);
+		}
+		return res;
 		
 	}
 
@@ -37,6 +57,31 @@ Set<Integer> set;
 	void containsTest() {
 		assertTrue(set.contains(60));
 		assertFalse(set.contains(80));
+	}
+	@Test
+	void iteratorNoRemoveTest() {
+		Integer[] randomNumbers = getRandomNumbers();
+		Set<Integer> numbersSet = new TreeSet<>();
+		fillSetFromArray(numbersSet, randomNumbers);
+		Arrays.sort(randomNumbers);
+		assertArrayEquals(randomNumbers, getArrayFromSet(numbersSet));
+	}
+	@Test
+	void treeSetInsensitiveOrderTest () {
+		 String strings[] = {"Boris", "Asaf", "android", "band"};
+		 String expected[] = {"android", "Asaf", "band", "Boris"};
+		 TreeSet<String> treeSet = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+		 fillSetFromArray(treeSet, strings);
+		 assertArrayEquals(expected, getArrayFromSet(treeSet));
+	}
+	@SuppressWarnings("unchecked")
+	private <T> T[] getArrayFromSet(Set<T> set) {
+		T res[] = (T[]) new Object[set.size()];
+		int ind = 0;
+		for(T obj: set) {
+			res[ind++] = obj;
+		}
+		return res;
 	}
 	
 
